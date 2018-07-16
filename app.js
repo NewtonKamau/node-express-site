8aeb2056d7ca8c4037a280e6dd16b4045c7e2927
Newton <kamaunewton78@gmail.com>
2018-07-16 16:46:22 +0300

    the basic front end of an express website

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var about = require('./routes/about');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/about', about);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
