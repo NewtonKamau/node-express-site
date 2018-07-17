df15ce8f770629efb8c7a796a3d896f82a76c5d2
Newton <kamaunewton78@gmail.com>
2018-07-17 07:45:15 +0300

    implementing node mailer

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+	res.render('contact', { title: 'Contact' });
 });
+
+router.post('/send', function(req, res, next){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'kamaunenwton78@gmail.com',
+			pass: 'Carina123c'
+		}
+	});
+	var mailOptions = {
+		from: 'Newnyc <kamaunenwnyc@gmail.com>',
+		to: 'kamaunenwton78@gmail.com',
+		subject: 'Test email',
+		text: 'You have a new submission with the following details..Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
+		html: '<p> You got a new submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li></ul>'
+	};
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		}else{
+			console.log('Message sent:' +info.response);
+			res.redirect('/');
+		}
+	})
+})
 
 module.exports = router;
